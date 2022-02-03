808dce18f6cf25cbbba36987c076bbb8eb5853cc
Yusuf Fakhreddin <48655310+Yusuf-Fakhreddin@users.noreply.github.com>
2022-02-03 17:28:24 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿namespace CargoShipLoader // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ship ship = new Ship();
+
+            string gameWelcomeMessage = "Welcome to Cargo Ship\nThe goal of this game is to load the ship to its maxiumum capacity by adding motor cycles, cars, trucks annd train cars.\n" +
+                "A cycle takes 3 units of weight.\n" +
+                "A car takes 5 units of weight\n" +
+                "A truck takes 11 units of weight\n" +
+                "A train car takes 17 units of weight\n\n";
+
+            Console.WriteLine(gameWelcomeMessage);
+
+            while (ship.getCurrentShipLoad()!= ship.MaxCapacity)
+            {
+                ship.CycleCount = 0;
+                ship.CarCount=0;
+                ship.TruckCount=0;
+                ship.TrainCount=0;
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("The ship maximum capacity is "+ship.MaxCapacity);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine("The ship has "+ ship.CycleCount +" motorcycles on board");
+                Console.WriteLine("The ship has " + ship.CarCount + " cars on board");
+                Console.WriteLine("The ship has " + ship.TruckCount + " trucks on board");
+                Console.WriteLine("The ship has " + ship.TrainCount + " train cars on board");
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("The ship has " + ship.getCurrentShipLoad() + " total weight units on board");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                int x;
+
+                Console.WriteLine("How many motor cycles would you like to put on the ship?") ;
+                x = int.Parse(Console.ReadLine());
+                ship.CycleCount=x;
+
+                Console.WriteLine("How many cars would you like to put on the ship?");
+                x = int.Parse(Console.ReadLine());
+                ship.CarCount = x;
+
+                Console.WriteLine("How many trucks would you like to put on the ship?");
+                x = int.Parse(Console.ReadLine());
+                ship.TruckCount = x;
+
+                Console.WriteLine("How many train cars would you like to put on the ship?");
+                x = int.Parse(Console.ReadLine());
+                ship.TrainCount = x;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("The ship current Load is" + ship.getCurrentShipLoad() + " weight units of load\n");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                int y = ship.howLoaded();
+                if (y > 0)
+                {
+                    Console.WriteLine(String.Format("The ship still has room. Try again and load {0} weight units more\n", y));
+                }
+                else if(ship.howLoaded() <0 )
+                {
+                    Console.WriteLine(String.Format("The ship is overloaded. Try again and take {0} weight units off\n", Math.Abs(y)));
+
+                }
+
+
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Congrats, You loaded the ship successfully!");
+        }
+    }
+}
---
+++ Ship.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CargoShipLoader
+{
+     class Ship
+    {
+        // const values
+        const int CYCLE_WEIGHT = 3;
+        const int CAR_WEIGHT = 5;
+        const int TRUCK_WEIGHT = 11;
+        const int TRAIN_WEIGHT = 17;
+
+        const int MAX_COUNT = 10;
+        
+
+        // class properties
+        public int MaxCapacity { get; set; }
+        public int CycleCount { get; set; }
+        public int TruckCount { get; set; }
+        public int CarCount { get; set; }
+        public int TrainCount { get; set; }
+
+        Random random = new Random();
+
+        // constructor
+        public Ship()
+        {
+            // properties initialization
+            CycleCount=0;  
+            TrainCount=0;
+            CarCount=0;
+            TruckCount=0;   
+
+            //random ship size
+            MaxCapacity = random.Next(MAX_COUNT)* (CYCLE_WEIGHT+TRAIN_WEIGHT+TRUCK_WEIGHT+CAR_WEIGHT)  ;
+
+        }
+
+        // methods
+        public int getCurrentShipLoad ()
+        {
+            return CycleCount*CYCLE_WEIGHT + TrainCount*TRAIN_WEIGHT + TruckCount*TRUCK_WEIGHT + CarCount*CAR_WEIGHT ;
+        }
+
+        public int howLoaded()
+        {
+            return MaxCapacity - getCurrentShipLoad();
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Max Capacity = {0} & Current Load = {1}", MaxCapacity , getCurrentShipLoad());
+        }
+    }
+}
